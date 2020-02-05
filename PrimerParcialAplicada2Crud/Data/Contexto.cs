9cf381a799d5ab8bd93572a8257186357f7ec511
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-02-05 13:53:09 -0400

    Agregando el ConnectionString y la carpeta Controllers

--- PrimerParcialAplicada2Crud/Data/Contexto.cs
+++ PrimerParcialAplicada2Crud/Data/Contexto.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace PrimerParcialAplicada2Crud.Data
 {
-    public class Contexto
+    public class Contexto :DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlServer(@"Server=DESKTOP-B3FBQ4F;Database=PrimerParcialAplicada2Db;Trusted_Connection=true"));
+        }
 
     }
 }
